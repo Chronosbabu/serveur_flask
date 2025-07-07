70652d583a9a998d992f385d725e8f070ecd0e67
babu bininga <biningababu4@gmail.com>
2025-07-07 22:31:47 +0200

    Mise à jour

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ messages = []
 
 @app.route('/')
 def accueil():
-    return "Serveur Flask sur Railway fonctionne !"
+    return "Serveur Flask avec ID client fonctionne !"
 
 @app.route('/envoyer', methods=['POST'])
 def envoyer():
-    data = request.json
-    messages.append(data)
-    return jsonify({"status": "Message reçu"})
+    data = request.get_json()
+    if "client" in data and "message" in data:
+        messages.append(data)
+        return jsonify({"status": "Message reçu"}), 200
+    return jsonify({"error": "Données invalides"}), 400
 
 @app.route('/recevoir', methods=['GET'])
 def recevoir():
@@ -19,3 +21,4 @@ def recevoir():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
